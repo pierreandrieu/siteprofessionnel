6699ec5de6d0d60d88baf1b7ee677dcf0da16793
andrieu <pierre.andrieu@lri.fr>
2025-08-24 22:25:38 +0200

    correction bug fichier robots.txt

--- sitepro/urls.py
+++ sitepro/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
 
     path(
         "robots.txt",
-        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+        TemplateView.as_view(template_name="pages/robots.txt", content_type="text/plain"),
         name="robots_txt",
     ),
 ]
