adb8d96e42e8091693385cc80f3eae23661062d2
andrieu <pierre.andrieu@lri.fr>
2025-08-30 04:10:30 +0200

    utilisation de tâches par celery

--- plandeclasse/apps.py
+++ plandeclasse/apps.py
@@ -1,6 +1,10 @@
+# plandeclasse/apps.py
 from django.apps import AppConfig
 
 
 class PlandeclasseConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "plandeclasse"
+
+    def ready(self):
+        from .contraintes import enregistrement
